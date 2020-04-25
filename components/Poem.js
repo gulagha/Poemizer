a326e0d4c41e63e5b6a53d1f41607cdecbc72404
Javid Museyibli <javid.museyibli@pashabank.az>
2020-04-25 15:05:20 +0400

    Separate SVGs from Poem.js & generalize styles.

--- components/Poem.js
+++ components/Poem.js
@@ -1,115 +1,121 @@
-import * as WebBrowser from 'expo-web-browser';
+import * as WebBrowser from "expo-web-browser";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import ThemeContext from "../ThemeContext";
+import NightIcon from "./svg/NightIcon";
+import DayIcon from "./svg/DayIcon";
 
-import React, { useContext, useEffect, useState } from 'react';
-import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import Svg, {Circle, Line, Path} from 'react-native-svg';
+export default function Poem({ poem, author, title, link }) {
+  const { theme, storeTheme } = useContext(ThemeContext);
+  const [fontSize, setFontSize] = useState(18);
+  const [themeStyles, setThemeStyles] = useState(styles);
 
-import ThemeContext from '../ThemeContext';
+  useEffect(() => {
+    if (theme == "light") setThemeStyles(styles);
+    else setThemeStyles(darkStyles);
+  }, [theme]);
 
-export default function Poem ({poem, author, title, link}) {
-    const {theme, storeTheme} = useContext(ThemeContext)
-    const [fontSize, setFontSize] = useState(18);
-    const [themeStyles, setThemeStyles] = useState(styles);
+  author.name = author.name.replace(/^\s+/g, "");
+  return (
+    <ScrollView style={{ height: "100%" }}>
+      <TouchableOpacity
+        style={styles.themeSwitcher}
+        onPress={() => storeTheme(theme == "light" ? "dark" : "light")}
+      >
+        {theme == "light" ? <NightIcon /> : <DayIcon />}
+      </TouchableOpacity>
+      <View>
+        <Text selectable style={themeStyles.title}>
+          {title}
+        </Text>
+        <Text
+          onPress={() => WebBrowser.openBrowserAsync(author.url)}
+          selectable
+          style={themeStyles.author}
+        >
+          By {author.name}
+        </Text>
+      </View>
 
-    useEffect(() => {
-        if(theme == "light") setThemeStyles(styles);
-        else setThemeStyles(darkStyles)
-    }, [theme])
-
-    author.name = author.name.replace(/^\s+/g, '')
-    return (
-        <ScrollView style={{height: "100%"}}>
-            <TouchableOpacity 
-                style={{marginTop: 45, alignItems:"flex-end", paddingRight: 20, backgroundColor: "transparent"}}
-                onPress={() => storeTheme(theme=="light" ? "dark" : "light")}>
-                {theme == "light"
-                    ? <Svg width="32" height="32">
-                        <Path 
-                            fill="none" 
-                            stroke="#000" 
-                            stroke-linecap="round" 
-                            stroke-linejoin="round" 
-                            stroke-width="5" 
-                            d="M20.63,20a9,9,0,0,1-9.12-8.78A8.61,8.61,0,0,1,14.17,5,10.17,10.17,0,0,0,5,15,10.23,10.23,0,0,0,15.42,25,10.43,10.43,0,0,0,25,18.9,9.3,9.3,0,0,1,20.63,20Z"/>
-                    </Svg>
-                    : <Svg width="32" height="32">
-                        <Circle cx="15" cy="15" r="5" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="15" x2="15" y1="5" y2="3" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="15" x2="15" y1="27" y2="25" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="5" x2="3" y1="15" y2="15" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="27" x2="25" y1="15" y2="15" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="7.93" x2="6.51" y1="22.07" y2="23.49" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="23.49" x2="22.07" y1="6.51" y2="7.93" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="22.07" x2="23.49" y1="22.07" y2="23.49" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                        <Line x1="6.51" x2="7.93" y1="6.51" y2="7.93" fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="5"/>
-                    </Svg>
-                }
-            </TouchableOpacity>
-            <View>
-                <Text selectable style={themeStyles.title}>{'\n' + title}</Text>
-                <Text onPress={() => WebBrowser.openBrowserAsync(author.url)} selectable style={themeStyles.author}>By {author.name}</Text>
-            </View>
-
-            <Text 
-                selectable
-                style={{...themeStyles.poem, fontSize}}> 
-                {'\n' + poem +'\n\n'} 
-            </Text>
-            <Text 
-                selectable
-                onPress={() => WebBrowser.openBrowserAsync(link)}
-                style={{...themeStyles.poem, color: "#ff8855"}}> 
-                {'Link \n\n\n\n\n\n\n\n'} 
-            </Text>
-        </ScrollView>
-
-    )
+      <Text selectable style={{ ...themeStyles.poem, fontSize }}>
+        {poem}
+      </Text>
+      <Text
+        selectable
+        onPress={() => WebBrowser.openBrowserAsync(link)}
+        style={themeStyles.link}
+      >
+        {"Link"}
+      </Text>
+    </ScrollView>
+  );
 }
 
+const commonStyles = {
+  placement: {
+    marginLeft: 30,
+    marginRight: 45,
+    textAlign: "left",
+  },
+};
+
 const styles = StyleSheet.create({
-    title:{
-        marginLeft: 30, 
-        marginRight: 45, 
-        textAlign:"left", 
-        fontSize: 21, 
-        fontWeight: "bold"
-    },
-    author: {
-        marginLeft: 30, 
-        marginRight: 45, 
-        textAlign:"left",
-        color: "#554444"
-    },
-    poem: {
-        marginLeft: 30, 
-        marginRight: 45, 
-        textAlign:"left", 
-        fontFamily: "random-regular", 
-        fontSize: 18,
-    }
-})
+  title: {
+    ...commonStyles.placement,
+    marginTop: 16,
+    fontSize: 21,
+    fontWeight: "bold",
+  },
+  author: {
+    ...commonStyles.placement,
+    color: "#554444",
+  },
+  poem: {
+    ...commonStyles.placement,
+    marginTop: 16,
+    marginBottom: 32,
+    fontFamily: "random-regular",
+    fontSize: 18,
+  },
+  link: {
+    ...commonStyles.placement,
+    marginTop: 16,
+    marginBottom: 120,
+    fontFamily: "random-regular",
+    fontSize: 18,
+    color: "#ff8855",
+  },
+  themeSwitcher: {
+    marginTop: 45,
+    paddingRight: 20,
+    alignItems: "flex-end",
+    backgroundColor: "transparent",
+  },
+});
 
 const darkStyles = StyleSheet.create({
-    title:{
-        marginLeft: 30, 
-        marginRight: 45, 
-        textAlign:"left", 
-        fontSize: 21, 
-        fontWeight: "bold",
-        color: "white"
-    },
-    author: {
-        marginLeft: 30, 
-        marginRight: 45, 
-        textAlign:"left",
-        color: "white",
-    },
-    poem: {
-        marginLeft: 30, 
-        marginRight: 45, 
-        textAlign:"left", 
-        fontFamily: "random-regular", 
-        fontSize: 18,
-        color: "white"
-    }
-})
+  title: {
+    ...styles.title,
+    color: "white",
+  },
+  author: {
+    ...styles.author,
+    color: "white",
+  },
+  poem: {
+    ...styles.poem,
+    color: "white",
+  },
+  link: {
+    ...styles.link,
+  },
+  themeSwitcher: {
+    ...styles.themeSwitcher,
+  },
+});
---
+++ components/svg/NightIcon.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import SvgUri from "react-native-svg-uri";
+export default () => (
+  <SvgUri
+    width="32"
+    height="32"
+    source={require("./../../assets/images/night.svg")}
+  />
+);
